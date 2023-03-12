b21ff450732400f98a97a033b26d1a0bb75ec209
ferranm99 <ferranm99@gmail.com>
2023-03-12 14:12:13 +0400

    refactor app component

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -2,19 +2,10 @@
   <Navbar />
 
   <main class="container">
-    <EditTodoForm
-      :show="editTodoForm.show"
-      @close="editTodoForm.show = false"
-      @submit="updateTodo"
-      v-model="editTodoForm.todo.title"
-    />
+    <EditTodoForm :show="editTodoForm.show" @close="editTodoForm.show = false" @submit="updateTodo"
+      v-model="editTodoForm.todo.title" />
 
-    <Alert 
-      :message="alert.message" 
-      :show="alert.show" 
-      :variant="alert.variant" 
-      @close="alert.show = false" 
-      />
+    <Alert :message="alert.message" :show="alert.show" :variant="alert.variant" @close="alert.show = false" />
 
     <section>
       <AddTodoForm :isLoading="isPostingTodo" @submit="addTodo" />
@@ -23,116 +14,99 @@
     <section>
       <Spinner class="spinner" v-if="isLoading" />
       <div v-else>
-        <Todo 
-          v-for="todo in todos" 
-          :key="todo.id" 
-          :title="todo.title" 
-          @remove="removeTodo(todo.id)"
+        <Todo v-for="todo in todos" :key="todo.id" :title="todo.title" @remove="removeTodo(todo.id)"
           @edit="showEditTodoForm(todo)" />
       </div>
     </section>
   </main>
 </template>
 
-<script>
+<script setup>
+//with keyword setup we dont need to create setup(){} with all the components and return...
+
 import Alert from "./components/Alert.vue";
 import Navbar from "./components/Navbar.vue";
 import AddTodoForm from "./components/AddTodoForm.vue";
 import Todo from "./components/Todo.vue";
-import Modal from "./components/Modal.vue";
-import Btn from "./components/Btn.vue";
 import Spinner from "./components/Spinner.vue";
 import axios from "axios";
 import EditTodoForm from "./components/EditTodoForm.vue";
+import { ref, reactive } from "vue";
 
-export default {
-  components: {
-    Alert,
-    Navbar,
-    AddTodoForm,
-    Todo,
-    Modal,
-    Btn,
-    Spinner,
-    EditTodoForm
-},
+const todos = ref([])
+const alert = reactive({
+  message: "",
+  show: false,
+  variant: "danger"
+})
 
-  data() {
-    return {
-      todoTitle: "",
-      todos: [],
-      alert: {
-        message: "",
-        show: false,
-        variant: "danger"
-      },
-      isLoading: false,
-      isPostingTodo: false,
-      editTodoForm: {
-        show: false,
-        todo: {
-          id: 0,
-          title: ""
-        }
-      }
-    };
-  },
-  created() {
-    this.fetchTodos()
-  },
+const isLoading = ref(false)
+const isPostingTodo = ref(false)
+const editTodoForm = reactive({
+  show: false,
+  todo: {
+    id: 0,
+    title: ""
+  }
+})
 
-  methods: {
-    async fetchTodos() {
-      //const res = await fetch('http://localhost:8080/todos')
-      this.isLoading = true;
-      try {
-        const res = await axios.get('/api/todos')
-        this.todos = res.data
-      } catch (e) {
-        this.showAlert("Request failed")
-      }
-      this.isLoading = false;
-    },
-    async addTodo(title) {
-      if (title.trim() === "") {
-        this.showAlert("Todo title required")
-        return;
-      } else {
-        this.alert.show = false;
-      }
-      this.isPostingTodo = true;
-      const res = await axios.post('/api/todos', { title })
-      this.isPostingTodo = false;
-      this.todos.push(res.data);
-    },
-    showAlert(m, variant = "danger") {
-      this.alert.message = m
-      this.alert.show = true;
-      this.alert.variant = variant
-    },
-    async removeTodo(id) {
-      await axios.delete('/api/todos/' + id)
-      //this.todos = this.todos.filter((todo) => todo.id !== id);
-      this.fetchTodos()
-    },
-    showEditTodoForm(todo) {
-      this.editTodoForm.show = true
-      this.editTodoForm.todo = { ...todo } //copies the data of object instead of reference
-    },
-    async updateTodo() {
-      this.isLoading = true;
-      try {
-        await axios.put('/api/todos/' + this.editTodoForm.todo.id, this.editTodoForm.todo)
-        const todo = this.todos.find(todo => todo.id === this.editTodoForm.todo.id)
-        todo.title = this.editTodoForm.todo.title
-      } catch (e) {
-        this.showAlert("Edit failed")
-      }
-      this.isLoading = false;
-      this.editTodoForm.show = false
-    }
-  },
-};
+async function fetchTodos() {
+  //const res = await fetch('http://localhost:8080/todos')
+  isLoading.value = true;
+  try {
+    const res = await axios.get('/api/todos')
+    todos.value = res.data
+  } catch (e) {
+    showAlert("Request failed")
+  }
+  isLoading.value = false;
+}
+
+async function addTodo(title) {
+  if (title.trim() === "") {
+    showAlert("Todo title required")
+    return;
+  } else {
+    alert.show = false;
+  }
+  isPostingTodo.value = true;
+  const res = await axios.post('/api/todos', { title })
+  isPostingTodo.value = false;
+  todos.value.push(res.data);
+}
+
+function showAlert(m, variant = "danger") {
+  alert.message = m
+  alert.show = true;
+  alert.variant = variant
+}
+
+async function removeTodo(id) {
+  await axios.delete('/api/todos/' + id)
+  //todos = todos.filter((todo) => todo.id !== id);
+  fetchTodos()
+}
+
+function showEditTodoForm(todo) {
+  editTodoForm.show = true
+  editTodoForm.todo = { ...todo } //copies the data of object instead of reference
+}
+
+async function updateTodo() {
+  isLoading.value = true;
+  try {
+    await axios.put('/api/todos/' + editTodoForm.todo.id, editTodoForm.todo)
+    const todo = todos.value.find(todo => todo.id === editTodoForm.todo.id)
+    todo.title = editTodoForm.todo.title
+  } catch (e) {
+    showAlert("Edit failed")
+  }
+  isLoading.value = false;
+  editTodoForm.show = false
+}
+
+fetchTodos()
+
 </script>
 
 <style scoped>
